4ab174b9dc8ddbfd0809c438284160172a02cdd1
Conrad Kriel <conradkriel7006@gmail.com>
2026-02-23 10:45:39 +0200

    Update Extracting the data.py

--- Extracting the data.py
+++ Extracting the data.py
@@ -158,7 +158,8 @@ df_5km_bobaas["Total"] = 225 - (15 - df_5km_bobaas).clip(lower=0).sum(axis=1)
 df_10km_rank_display = df_10km_rank.fillna(999)
 df_5km_rank_display = df_5km_rank.fillna(999)
 
-
+#print(df_10km_rank.head())
+#print(df_10km_rank_display.head())
 
 # %% #
 
@@ -179,10 +180,10 @@ if menu == "Main":
 
     # Show the two rank dataframes
     st.write("The results of our 10 km dam dices")
-    st.dataframe(df_10km_rank)   # The format code is used to display correctly
+    st.dataframe(df_10km_rank_display.style.format(lambda x: "" if x == 999 else int(x)))   # The format code is used to display correctly
     
     st.write("\nThe results of our 5 km dam dices")
-    st.dataframe(df_5km_rank)
+    st.dataframe(df_5km_rank_display.style.format(lambda x: "" if x == 999 else int(x)))
 
 ### Next, the page that contains information on the Yster competition
 elif menu == "Yster":
