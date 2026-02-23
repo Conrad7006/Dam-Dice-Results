5d7b95507c0009b0bd1b6373f35ea7f14b643537
Conrad Kriel <conradkriel7006@gmail.com>
2026-02-23 10:30:39 +0200

    Update Extracting the data.py

--- Extracting the data.py
+++ Extracting the data.py
@@ -179,10 +179,10 @@ if menu == "Main":
 
     # Show the two rank dataframes
     st.write("The results of our 10 km dam dices")
-    st.dataframe(df_10km_rank.style.format(lambda x: "" if x == 999 else int(x)))   # The format code is used to display correctly
+    st.dataframe(df_10km_rank.style.format(lambda x: "" if x == 999 else x))   # The format code is used to display correctly
     
     st.write("\nThe results of our 5 km dam dices")
-    st.dataframe(df_5km_rank.style.format(lambda x: "" if x == 999 else int(x)))
+    st.dataframe(df_5km_rank.style.format(lambda x: "" if x == 999 else x))
 
 ### Next, the page that contains information on the Yster competition
 elif menu == "Yster":
